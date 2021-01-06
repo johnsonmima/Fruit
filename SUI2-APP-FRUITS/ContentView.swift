1fbbf38ebaeeef00a092191ee7c146ece0443030
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2021-01-06 08:22:13 -0500

    Initial Commit

---
+++ SUI2-APP-FRUITS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUI2-APP-FRUITS
+//
+//  Created by Johnson Olusegun on 1/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SUI2-APP-FRUITS/SUI2_APP_FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI2_APP_FRUITSApp.swift
+//  SUI2-APP-FRUITS
+//
+//  Created by Johnson Olusegun on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SUI2_APP_FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
